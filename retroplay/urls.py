c2f6be4fdd1ad42d6f5673f7ceccd8cea5b46f7b
Michael Park <mparkcode@gmail.com>
2018-08-01 12:44:29 +0000

    Navbar styling & initial styling on index

--- retroplay/urls.py
+++ retroplay/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from products.views import index
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
