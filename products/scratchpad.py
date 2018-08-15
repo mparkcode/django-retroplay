b9a0fbd55bcf6e1b7e17e1052bb2408d49d090fb
Michael Park <mparkcode@gmail.com>
2018-08-15 07:48:27 +0000

    updated db

--- products/scratchpad.py
+++ products/scratchpad.py
@@ -1,3 +1,7 @@
-from .models import Game
-Game.objects.all().delete()
+from .models import Game, Console
+console_name = Console.objects.get(console_type="mega-cd")
+# console_name = Console.objects.get(console_type="master-system")
+# print(console_name)
+Game.objects.filter(console=console_name).delete()
+
 # Game.objects.filter(image="https://www.consolemad.co.uk/wp-content/uploads/2018/07/James-Bond-007-–-Agent-Under-Fire-250x322.jpg").delete()
