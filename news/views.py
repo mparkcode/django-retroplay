6df85636a9f84eff9917832c57f02f75178820d0
Michael Park <mparkcode@gmail.com>
2018-08-15 13:01:52 +0000

    styling and pagination on all_news.html

--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Article
+from django.core.paginator import Paginator
 
 # Create your views here.
 def all_news(request):
@@ -8,8 +9,15 @@ def all_news(request):
         query = search_query
         return redirect('search_results', query)
     articles = Article.objects.all()
+    paginator = Paginator(articles, 5)
+    page = request.GET.get('page')
+    articles=paginator.get_page(page)
     return render(request, "news/all_news.html", {'articles':articles})
     
 def view_article(request, pk):
+    search_query = request.GET.get("query")
+    if search_query:
+        query = search_query
+        return redirect('search_results', query)
     article = Article.objects.get(pk=pk)
     return render(request, "news/view_article.html", {'article':article})
