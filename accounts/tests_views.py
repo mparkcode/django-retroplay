22e8753445bd38cf467879c9d3d8cf4b0519d929
Michael Park <mparkcode@gmail.com>
2018-08-28 08:40:15 +0000

    added hover.css effects on nav, fixed bugs for missing fonts & files

--- accounts/tests_views.py
+++ accounts/tests_views.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .forms import UserRegistrationForm
 
 # Create your tests here.
 class TestAccountsViews(TestCase):
@@ -23,5 +25,39 @@ class TestAccountsViews(TestCase):
         self.assertRedirects(response, '/', status_code=302, 
         target_status_code=200, fetch_redirect_response=True)
         
-    
+    def test_user_can_log_in(self):
+        test_user = User.objects.create_user(username="test", email="test@example.com", password="Madetotest")
+        response = self.client.post("/accounts/login", {
+            'username': 'test',
+            'password': 'Madetotest'
+        })
+        self.assertRedirects(response, '/', status_code=302, 
+        target_status_code=200, fetch_redirect_response=True)
         
+    def test_error_on_login(self):
+        response = self.client.post("/accounts/login", {
+            'username': 'test',
+            'password': 'Madetotest'
+        })
+        self.assertFormError(response, 'form', None, "Your username or password are incorrect")
+        
+    def test_user_can_register(self):
+        response = self.client.post("/accounts/register", {
+            'username': 'test',
+            'email': 'test@email.com',
+            'password1': 'Madetotest',
+            'password2': 'Madetotest'
+        })
+        self.assertRedirects(response, '/', status_code=302, 
+        target_status_code=200, fetch_redirect_response=True)
+        
+    def test_error_on_registration(self):
+        form_params={
+            'username': 'test',
+            'email': 'test@email.com',
+            'password1': 'Madtotest',
+            'password2': 'Madetotest'
+        }
+        response = self.client.post("/accounts/register", form_params)
+        form = UserRegistrationForm(form_params)
+        self.assertFalse(form.is_valid())
